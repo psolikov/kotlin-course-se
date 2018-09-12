a00a6be045f4a43441ec25356a0d075ba66396db
Pavel Solikov <psolikov15@gmail.com>
2018-09-12 10:09:28 +0300

    Done JFF

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,106 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
+import kotlin.collections.ArrayList
 
-    return words.joinToString(separator = " ")
+class Graph(_size: Int) {
+    private val g: ArrayList<ArrayList<Int>> = ArrayList()
+    private val cycle: ArrayList<Int> = ArrayList()
+    private val distances: ArrayList<Pair<Int, Int>> = ArrayList()
+    private val size: Int = _size
+    private val parent: ArrayList<Int> = ArrayList()
+    private val color: ArrayList<Int> = ArrayList()
+    private var cycleStart: Int = -1
+    private var cycleEnd: Int = -1
+
+    init {
+        for (i in 0 until size) {
+            g.add(ArrayList())
+            parent.add(-1)
+            color.add(0)
+        }
+    }
+
+    fun addEdge(from: Int, to: Int) {
+        g[from - 1].add(to - 1)
+        g[to - 1].add(from - 1)
+    }
+
+    fun findCycle() {
+        dfs(0)
+        cycle.add(cycleStart)
+        var v = cycleEnd
+        while (v != cycleStart) {
+            cycle.add(v)
+            v = parent[v]
+        }
+    }
+
+    fun findDistances() {
+        distances.addAll(cycle.map { v -> Pair(v, 0) })
+        cycle.forEach { v -> bfs(v) }
+        distances.sortBy { pair -> pair.first }
+    }
+
+    fun printDistances() {
+        distances.forEach { pair -> print(pair.second); print(" ") }
+    }
+
+    private fun dfs(v: Int): Boolean {
+        color[v] = 1
+        for (i in 0 until g[v].size) {
+            val to: Int = g[v][i]
+            if (color[to] == 0) {
+                parent[to] = v
+                if (dfs(to)) {
+                    return true
+                }
+            } else if (color[to] == 1 && to != parent[v]) {
+                cycleEnd = v
+                cycleStart = to
+                return true
+            }
+        }
+        color[v] = 2
+        return false
+    }
+
+    private fun bfs(s: Int) {
+        val q = ArrayDeque<Int>()
+        q.push(s)
+        val used = BooleanArray(size)
+        val d = IntArray(size)
+        val p = IntArray(size)
+        used[s] = true
+        p[s] = -1
+        while (!q.isEmpty()) {
+            val v = q.first()
+            q.pop()
+            for (i in 0 until g[v].size) {
+                val to = g[v][i]
+                if (cycle.contains(to)) continue
+                if (!used[to]) {
+                    used[to] = true
+                    q.push(to)
+                    d[to] = d[v] + 1
+                    distances.add(Pair(to, d[to]))
+                    p[to] = v
+                }
+            }
+        }
+    }
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val reader = Scanner(System.`in`)
+    val n: Int = reader.nextInt()
+    val graph = Graph(n)
+    for (i in 0 until n) {
+        val from: Int = reader.nextInt()
+        val to: Int = reader.nextInt()
+        graph.addEdge(from, to)
+    }
+    graph.findCycle()
+    graph.findDistances()
+    graph.printDistances()
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,51 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.io.ByteArrayOutputStream
+import org.junit.After
+import java.io.PrintStream
+import org.junit.Before
 
-class TestSource {
+
+class Tests {
+    private val outContent = ByteArrayOutputStream()
+    private val originalOut = System.out
+
+    @Before
+    fun setUpStreams() {
+        System.setOut(PrintStream(outContent))
+    }
+
+    @After
+    fun restoreStreams() {
+        System.setOut(originalOut)
+    }
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testSimple1() {
+        val graph = Graph(4)
+        graph.addEdge(1, 3)
+        graph.addEdge(4, 3)
+        graph.addEdge(4, 2)
+        graph.addEdge(1, 2)
+        graph.findCycle()
+        graph.findDistances()
+        graph.printDistances()
+        assert(outContent.toString() == "0 0 0 0 ")
+    }
+
+    @Test
+    fun testSimple2() {
+        val graph = Graph(6)
+        graph.addEdge(1, 2)
+        graph.addEdge(3, 4)
+        graph.addEdge(6, 4)
+        graph.addEdge(2, 3)
+        graph.addEdge(1, 3)
+        graph.addEdge(3, 5)
+        graph.findCycle()
+        graph.findDistances()
+        graph.printDistances()
+        assert(outContent.toString() == "0 0 0 1 1 2 ")
     }
 }
